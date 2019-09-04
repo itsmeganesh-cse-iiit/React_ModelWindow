3f7a3ad6dd779c0f756013a1c983d46e9dc60038
itsmeganesh-cse-iiit <itsmeganeshcse@gmail.com>
2019-09-05 00:34:58 +0530

    google map embed url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Modal from 'react-bootstrap/Modal'
 import {Button,ButtonToolbar} from 'react-bootstrap'
 import { useState } from 'react';
-import Iframe from 'react-iframe'
 function Example() {
   const [lgShow, setLgShow] = useState(false);
 
@@ -22,8 +21,7 @@ function Example() {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        {/* <iframe src="https://www.google.com/maps/place/Sphere+Soft+Solutions+India+Pvt.+Ltd./@17.4416921,78.4731463,17z/data=!4m18!1m12!4m11!1m3!2m2!1d78.4752492!2d17.4418507!1m6!1m2!1s0x3bcb9a082a4f16f1:0x499d0acedbf40eef!2sSphere+Soft+Solutions+India+Pvt.+Ltd.,+1-8-313,+%233rd+Floor,,+Chiran+Fort+Club+Lane,+Opp.+American+Consulate,+Begumpet,+Hyderabad,+Telangana+500003!2m2!1d78.475335!2d17.441687!3m4!1s0x3bcb9a082a4f16f1:0x499d0acedbf40eef!8m2!3d17.441687!4d78.475335" width="100%" height="500px"></iframe> */}
-          <iframe title="googleMapAddress" src="http://www.youtube.com/embed/xDMP3i36naA" width="100%" height="500px" frameBorder="0"></iframe>
+          <iframe title="googleMapAddress" src="https://maps.google.com/maps?q=spheresoft%20solutions&t=&z=13&ie=UTF8&iwloc=&output=embed" width="100%" height="500px" frameBorder="0" ></iframe>
         </Modal.Body>
       </Modal>
     </ButtonToolbar>
